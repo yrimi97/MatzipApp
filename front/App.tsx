d1594d63ef07dda34d6de83e0f4cd8bf455ae2ac
조유림 <yrimi97@gmail.com>
2024-11-30 16:32:19 +0900

    2-2

--- front/App.tsx
+++ front/App.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { SafeAreaView, StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { useState } from "react";
 
 function App() {
+
+  const [name, setName] = useState('');
+
+  // const handleChangeInput = (e) => {
+  //   setName(e.target.value);
+  // };
+
+  // onChangeText 함수와 결합하면 e 이벤트 사용할 필요 없음
+  const handleChangeInput = (text: string) => {
+    console.log(text);
+    setName(text);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.input} />
+        <TextInput style={styles.input} value={name} onChangeText={handleChangeInput} />
         <Text>텍스트</Text>
       </View>
     </SafeAreaView>
